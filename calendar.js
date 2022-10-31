563643c5048f93e42d7069e9c1feb437bb7f5895
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-31 23:48:49 +0300

    setup

---
+++ calendar.js
@@ -0,0 +1,77 @@
+const choice =  document.querySelector('#choice-sel')
+const list = document.querySelector('.list-cal')
+const calendar = document.querySelector('.calendar')
+
+const calenderDays = (val, days) => {
+  const para = document.querySelector('.para')
+  para.textContent = val
+  calendar.appendChild(para)
+  for (let i = 1; i <= days; i++) {
+    let listItem = document.createElement('li')
+    listItem.textContent = i
+    list.appendChild(listItem)
+  }
+}
+
+
+
+const completeChange = () => {
+  const val = choice.value
+  let days
+  switch(val) {
+    case 'January':
+      days = 31
+      calenderDays(val, days)
+      break;
+    case 'February':
+      days = 28
+      calenderDays(val, days)
+      break
+    case 'March':
+      days = 31
+      calenderDays(val, days)
+      break
+    case 'April':
+      days = 30
+      calenderDays(val, days)
+      break;
+    case 'May':
+      days = 31
+      calenderDays(val, days)
+      break;
+    case 'June':
+      days = 30
+      calenderDays(val, days)
+      break;
+    case 'July':
+      days = 31
+      calenderDays(val, days)
+      break;
+    case 'August':
+      days = 31
+      calenderDays(val, days)
+      break;
+    case 'September':
+      days = 30
+      calenderDays(val, days)
+      break;
+    case 'October':
+      days = 31
+      calenderDays(val, days)
+      break;
+    case 'November':
+      days = 30
+      calenderDays(val, days)
+      break;
+    case 'December':
+      days = 31
+      calenderDays(val, days)
+      break;
+    default:
+      'Enter a valid month'
+  }
+}
+
+choice.addEventListener('change', completeChange)
+
+
